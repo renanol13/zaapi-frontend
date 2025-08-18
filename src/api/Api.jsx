d0aa2a8a119aada8edd012d5e5d1bb0b7ac7e682
Renan <renan13.com@gmailcom>
2025-08-17 21:10:09 -0300

    terminando logica de envio do conteudo do post para o servidor

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -26,8 +26,8 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     
-    const message = error.response?.data?.message || "Erro desconhecido";
-    return Promise.reject({ message: message, status: error.response?.status });
+    const message = error.response?.data?.message || "Erro desconhecido!";
+    return Promise.reject({ message: message, status: error.response?.status, error });
   }
 );
 
--- src/components/posts/NewPostWindow.jsx
+++ src/components/posts/NewPostWindow.jsx
@@ -5,22 +5,35 @@ import ButtonForm from "../../components/ButtonForm";
 import { IoClose } from "react-icons/io5";
 import UseAuth from "../../hooks/UseAuth";
 import { useEffect, useRef, useState } from "react";
+import api from "../../api/Api";
 
 const NewPostWindow = ({ setHandleWindowPost }) => {
   const [animatedDiv, setAnimatedDiv] = useState(false);
+  const [dataPost, setDataPost] = useState("");
   const textAreaFocus = useRef();
 
   const {
     dataUser: { name },
   } = UseAuth();
 
+  //Espera para realizar animação
   useEffect(() => {
     textAreaFocus.current?.focus();
 
-    setTimeout(() => {
-      setAnimatedDiv(true);
-    }, 10);
+    setAnimatedDiv(true);
   }, []);
+
+  const sendPost = async () => {
+    console.log("oii");
+
+    try {
+      const response = await api.post("/posts/create", { content: dataPost });
+      setHandleWindowPost(false);
+      console.log(dataPost);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <>
@@ -37,8 +50,10 @@ const NewPostWindow = ({ setHandleWindowPost }) => {
           <textarea
             placeholder={`Quais as novidades por aí, ${name} ?`}
             ref={textAreaFocus}
+            value={dataPost}
+            onChange={(e) => setDataPost(e.target.value)}
           ></textarea>
-          <ButtonForm text="Postar" />
+          <ButtonForm text="Postar" handleClick={() => sendPost()} />
         </div>
       </Container>
     </>
